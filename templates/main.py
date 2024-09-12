5682b5ce429fd176892331b8a4993cf2d2598c4c
QUEEN MASHUDU MUDAU <shudaumudau@gmail.com>
2024-09-12 23:39:41 +0200

    a

---
+++ templates/main.py
@@ -0,0 +1,5 @@
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from  src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
