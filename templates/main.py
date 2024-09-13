da3b209f2b99d1343abd7f68771daffe3fff0378
QUEEN MASHUDU MUDAU <shudaumudau@gmail.com>
2024-09-13 15:02:06 +0200

    utility

--- templates/main.py
+++ templates/main.py
@@ -1,5 +1,7 @@
-from cnnClassifier import logger
-from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.utils import logger
+
+# from cnnClassifier.pipeline.stage_01_data_ingestion 
+# import DataIngestionTrainingPipeline
 # from  src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-# from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
-# from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+# # from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+# # from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
